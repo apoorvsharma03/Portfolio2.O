eae5eff2daee1b5d0ad0d1803b89b7f8fc850a5d
Apoorv <apoorvsharma2603@gmail.com>
2025-04-01 15:14:39 +0530

    Animated Headings

--- src/assets/AboutMe.jsx
+++ src/assets/AboutMe.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import ContentHeading from '../utilities/ContentHeading'
 
 const AboutMe = () => {
   return (
     <div id='ABOUT ME' className='flex justify-center pt-5'>
       <div className='w-[80vw] md:w-[60vw]'>
-        <h1 className='font-semibold text-lg text-[#0a192f] bg-cyan-700 p-2'>ABOUT ME</h1>
+        <ContentHeading>ABOUT ME</ContentHeading>
         <br />
         <p className='text-cyan-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero cum rerum excepturi? Obcaecati a nihil iusto quam repellat similique itaque, quae, tempora minus vel, commodi sit? Dolor, alias voluptate architecto eligendi iure nostrum earum, ex praesentium porro quisquam veritatis quas?</p>
         <br />
---
+++ src/utilities/ContentHeading.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const ContentHeading = ({ children }) => {
+  const headingRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        threshold: 0.1,
+        rootMargin: '0px 0px -100px 0px'
+      }
+    );
+
+    if (headingRef.current) {
+      observer.observe(headingRef.current);
+    }
+
+    return () => {
+      if (headingRef.current) {
+        observer.unobserve(headingRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <h1 
+      ref={headingRef}
+      className={`font-semibold text-lg text-[#0a192f] p-2 relative overflow-hidden`}
+      style={{
+        position: 'relative',
+      }}
+    >
+      {children}
+      <div 
+        className={`absolute inset-0 bg-cyan-700 transition-transform duration-700 ease-out`}
+        style={{
+          transform: isVisible ? 'translateX(0)' : 'translateX(-100%)',
+          zIndex: -1,
+        }}
+      />
+    </h1>
+  );
+};
+
+export default ContentHeading;
