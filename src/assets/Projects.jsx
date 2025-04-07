6270aaf412bfba973f96cd4c2ed53021debe39cf
Apoorv <apoorvsharma2603@gmail.com>
2025-04-07 20:39:47 +0530

    Updated Experiences

--- src/assets/Projects.jsx
+++ src/assets/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import ContentHeading from '../utilities/ContentHeading'
 import ProjectCard from '../utilities/ProjectCard'
 import Snap_AI_Poster from "../utilities/Images/snap_ai.png"
@@ -6,10 +7,16 @@ import Cinemareel_Poster from "../utilities/Images/cinemareel.png"
 import Potfolio_Poster from "../utilities/Images/portfolio.png"
 
 const Projects = () => {
-  const Project1 = ["SNAP AI", Snap_AI_Poster, "https://snapaiproject.netlify.app", "SnapAI lets users generate AI images from prompts and explore a gallery of community-created, AI-generated artworks in various styles."]
-  const Project2 = ["PORTFOLIO", Potfolio_Poster, "", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. At corrupti, dolorem officia eligendi temporibus harum non vel quam voluptatum numquam?"]
-  const Project3 = ["CINEMAREEL", Cinemareel_Poster, "https://cinema-reel.netlify.app/", "CinemaReel is a fully responsive React-based frontend movie database featuring trending movies, a functional search bar, and IMDb redirect links."]
-  const Projects = [Project1, Project2, Project3]
+  const [selectedProject, setselectedProject] = useState(null)
+
+  const Project1 = {id:1, title:"SNAP AI", poster:Snap_AI_Poster, link:"https://snapaiproject.netlify.app", text:"SnapAI is a responsive platform for AI image generation from prompts, enabling users to create and share their artwork. The platform features a community gallery for browsing and downloading shared images. A search bar facilitates easy image discovery within the community."}
+  const Project2 = {id:2, title:"PORTFOLIO", poster:Potfolio_Poster, link:"", text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolorum at vero possimus excepturi porro? Earum quae perspiciatis aspernatur alias mollitia! Quia dignissimos possimus odit voluptatibus magni consequatur, ullam sit vel mollitia temporibus voluptate iusto officia modi veniam commodi maxime?"}
+  const Project3 = {id:3, title:"CINEMAREEL", poster:Cinemareel_Poster, link:"https://cinema-reel.netlify.app/", text:"CinemaReel is a user-friendly, responsive platform for discovering trending films. Users can explore detailed information about each movie and seamlessly navigate to its IMDb page via provided links. The platform boasts a clean, intuitive interface complemented by a fully functional search bar for easy navigation."}
+  const ProjectsList = [Project1, Project2, Project3]
+
+  const handleCardClick = (Project) =>{
+    selectedProject?.id === Project.id ? setselectedProject(null) : setselectedProject(Project)
+  }
 
   return (
     <div id='PROJECTS' className='flex justify-center pt-4'>
@@ -17,9 +24,14 @@ const Projects = () => {
         <div className='bg-transparent'>
           <ContentHeading>PROJECTS</ContentHeading>
         </div>
-        <div className='flex flex-wrap justify-around items-center gap-4 mt-4'>
-          {Projects.map((Project) => (
-            <ProjectCard props={Project} />
+        <div className='flex flex-wrap justify-center gap-4 mt-4 w-full'>
+          {ProjectsList.map((Project) => (
+              <ProjectCard
+                key={Project.id} 
+                props={Project}
+                onClick={()=>handleCardClick(Project)}
+                isFocused={selectedProject?.id === Project.id}
+              />
           ))}
         </div>
       </div>
--- src/utilities/ProjectCard.jsx
+++ src/utilities/ProjectCard.jsx
@@ -1,36 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import IsTouchable from "./TouchContext";
-import { IonIcon } from "@ionic/react";
-import { linkOutline } from "ionicons/icons";
 
-const ProjectCard = ({ props }) => {
-  const [websiteName, websitePoster, websiteLink, websiteText] = props;
-  const [isFocused, setIsFocused] = useState(false);
+const ProjectCard = ({ props, onClick, isFocused }) => {
+  const [maxPermissibleWidth, setmaxPermissibleWidth] = useState(0)
   const isTouchable = IsTouchable();
+
+  useEffect(() => {
+    const updateMaxWidth = () => {
+      const ProjectCard = document.getElementById('PROJECT CARD');
+      if (ProjectCard) {
+        const ProjectCardWidth = ProjectCard.offsetWidth;
+        setmaxPermissibleWidth(`calc(60vw - ${ProjectCardWidth}px - ${20*16}px)`);
+      }
+    };
+  
+    updateMaxWidth();
+    window.addEventListener("resize", updateMaxWidth);
+  
+    return () => window.removeEventListener("resize", updateMaxWidth);
+  }, []);
+  
 
   return (
     <div 
-      className="relative w-64 p-2 rounded-2xl bg-opacity-20 bg-cyan-900 shadow-2xl transition-all duration-300 overflow-hidden"
-      onMouseEnter={() => !isTouchable && setIsFocused(true)}
-      onMouseLeave={() => !isTouchable && setIsFocused(false)}
-      onClick={() => isTouchable && setIsFocused(!isFocused)}
+      id="PROJECT CARD" 
+      onClick={onClick} 
+      // style={{ maxWidth: maxPermissibleWidth }} 
+      className={`flex flex-col justify-center items-center p-3 cursor-pointer rounded-2xl ${isTouchable ? 'bg-opacity-20 bg-cyan-900 shadow-2xl min-w-fit' : 'bg-opacity-20 bg-cyan-900 shadow-xl hover:bg-opacity-40 hover:shadow-2xl transition-all md:flex-row'} ${isFocused && 'bg-opacity-40 shadow-2xl md:px-4 min-w-[20vw]'}`}
     >
-      <img 
-        src={websitePoster} 
-        alt="Project preview" 
-        className={`w-64 h-64 object-cover mx-auto rounded-2xl`}
-      />
-      
-      <div 
-        className={`absolute bottom-4 left-4  w-56 h-60 rounded-2xl bg-[#0a314a] transition-all ease-in-out duration-300 transform ${
-          isFocused ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'
-        }`}
-      >
-        <div className="flex flex-col items-center justify-between p-4 h-full w-full rounded-[0.85rem] overflow-hidden">
-            <h1 className="text-cyan-400 font-semibold text-lg">{websiteName}</h1>
-            <p className="text-cyan-600 leading-snug w-full overflow-hidden text-base">{websiteText}</p>
-            <a href={websiteLink} className="w-full flex justify-center bg-[#0a192f] rounded-2xl"><IonIcon icon={linkOutline} className="text-2xl flex text-cyan-400"></IonIcon></a>
+      <div className={`relative overflow-hidden transition-all duration-300 md:duration-200 ease-in-out md:ease-in text-cyan-600 ${!isTouchable && (isFocused ? 'max-w-[36vw] h-40 delay-100' : 'max-w-0 h-40')} ${isTouchable && (isFocused ? 'max-h-[500px] w-72 delay-100' : 'max-h-0 w-72') }`}>
+        <h1 className="text-cyan-400 font-semibold">{props.title}</h1>
+        <hr className="p-1 border-cyan-200"/>
+        <p className="pb-2 md:pr-2 mb-8 tracking-tight leading-[1.3rem]">{props.text}</p>
+        <div className={`absolute bottom-1 w-full ${isTouchable && 'mt-2'}`}>
+          <a href={props.link} className="rounded-xl px-3 py-1 font-semibold text-sm text-[#0a192f] bg-cyan-600 hover:bg-[#0a192f] hover:text-cyan-600 transition-all duration-100 ease-in-out">VISIT SITE!</a>
         </div>
+      </div>
+      <div className={`flex flex-col gap-2 items-center justify-center w-72 transition-all duration-300 ease-in-out overflow-hidden ${isFocused && (isTouchable ? 'mt-4' : 'w-0 opacity-0')}`}>
+        <img src={props.poster} alt={props.title} className='rounded-xl'/>
+        <h1 className='text-cyan-600 font-semibold tracking-wide'>{props.title}</h1>
       </div>
     </div>
   );
