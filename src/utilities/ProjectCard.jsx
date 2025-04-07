93cc0ffe5c6c2c7ab91cf23be315a2ca835af545
Apoorv <apoorvsharma2603@gmail.com>
2025-04-07 20:49:38 +0530

    Updated Experiences

--- src/utilities/ProjectCard.jsx
+++ src/utilities/ProjectCard.jsx
@@ -25,8 +25,7 @@ const ProjectCard = ({ props, onClick, isFocused }) => {
     <div 
       id="PROJECT CARD" 
       onClick={onClick} 
-      // style={{ maxWidth: maxPermissibleWidth }} 
-      className={`flex flex-col justify-center items-center p-3 cursor-pointer rounded-2xl ${isTouchable ? 'bg-opacity-20 bg-cyan-900 shadow-2xl min-w-fit' : 'bg-opacity-20 bg-cyan-900 shadow-xl hover:bg-opacity-40 hover:shadow-2xl transition-all md:flex-row'} ${isFocused && 'bg-opacity-40 shadow-2xl md:px-4 min-w-[20vw]'}`}
+      className={`flex flex-col justify-center items-center p-3 cursor-pointer rounded-2xl ${isTouchable ? 'bg-opacity-20 bg-cyan-900 shadow-2xl min-w-fit' : 'bg-opacity-20 bg-cyan-900 shadow-xl hover:bg-opacity-40 hover:shadow-2xl transition-all md:flex-row'} ${isFocused && 'bg-opacity-40 shadow-2xl md:px-4 min-w-80'}`}
     >
       <div className={`relative overflow-hidden transition-all duration-300 md:duration-200 ease-in-out md:ease-in text-cyan-600 ${!isTouchable && (isFocused ? 'max-w-[36vw] h-40 delay-100' : 'max-w-0 h-40')} ${isTouchable && (isFocused ? 'max-h-[500px] w-72 delay-100' : 'max-h-0 w-72') }`}>
         <h1 className="text-cyan-400 font-semibold">{props.title}</h1>
@@ -36,8 +35,8 @@ const ProjectCard = ({ props, onClick, isFocused }) => {
           <a href={props.link} className="rounded-xl px-3 py-1 font-semibold text-sm text-[#0a192f] bg-cyan-600 hover:bg-[#0a192f] hover:text-cyan-600 transition-all duration-100 ease-in-out">VISIT SITE!</a>
         </div>
       </div>
-      <div className={`flex flex-col gap-2 items-center justify-center w-72 transition-all duration-300 ease-in-out overflow-hidden ${isFocused && (isTouchable ? 'mt-4' : 'w-0 opacity-0')}`}>
-        <img src={props.poster} alt={props.title} className='rounded-xl'/>
+      <div className={`flex flex-col gap-2 items-center justify-center transition-all duration-300 ease-in-out overflow-hidden ${isTouchable? (isFocused && 'mt-4') : (isFocused ? 'w-0 opacity-0' : 'w-72')}`}>
+        <img src={props.poster} alt={props.title} className={`rounded-xl`}/>
         <h1 className='text-cyan-600 font-semibold tracking-wide'>{props.title}</h1>
       </div>
     </div>
