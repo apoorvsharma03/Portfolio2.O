b864c05829446002cd1d6082c722ccc40405ae13
Apoorv <apoorvsharma2603@gmail.com>
2025-04-10 19:04:48 +0530

    Updated Contact and Projects

--- src/assets/Projects.jsx
+++ src/assets/Projects.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import { useState } from 'react'
+import IsTouchable from '../utilities/TouchContext'
 import ContentHeading from '../utilities/ContentHeading'
 import ProjectCard from '../utilities/ProjectCard'
 import Snap_AI_Poster from "../utilities/Images/snap_ai.png"
 import Cinemareel_Poster from "../utilities/Images/cinemareel.png"
 import Potfolio_Poster from "../utilities/Images/portfolio.png"
+import Litsocbitm_Poster from "../utilities/Images/litsocbitm.png"
 
 const Projects = () => {
+  const isTouchable = IsTouchable()
   const [selectedProject, setselectedProject] = useState(null)
 
   const Project1 = {id:1, title:"SNAP AI", poster:Snap_AI_Poster, link:"https://snapaiproject.netlify.app", text:"SnapAI is a responsive platform for AI image generation from prompts, enabling users to create and share their artwork. The platform features a community gallery for browsing and downloading shared images. A search bar facilitates easy image discovery within the community."}
   const Project2 = {id:2, title:"PORTFOLIO", poster:Potfolio_Poster, link:"", text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolorum at vero possimus excepturi porro? Earum quae perspiciatis aspernatur alias mollitia! Quia dignissimos possimus odit voluptatibus magni consequatur, ullam sit vel mollitia temporibus voluptate iusto officia modi veniam commodi maxime?"}
-  const Project3 = {id:3, title:"CINEMAREEL", poster:Cinemareel_Poster, link:"https://cinema-reel.netlify.app/", text:"CinemaReel is a user-friendly, responsive platform for discovering trending films. Users can explore detailed information about each movie and seamlessly navigate to its IMDb page via provided links. The platform boasts a clean, intuitive interface complemented by a fully functional search bar for easy navigation."}
-  const ProjectsList = [Project1, Project2, Project3]
+  const Project3 = {id:4, title:"LITSOC, BITM Website", poster:Litsocbitm_Poster, link:"https://litsocbitm.netlify.app", text:"The official website for the Literary Society of BIT Mesra. Features multiple pages with smooth routing, a fully responsive layout, and an appealing image carousel. The dark-themed UI highlights events, publications, and members—enhancing user engagement and improving the society’s online presence."}
+  const Project4 = {id:3, title:"CINEMAREEL", poster:Cinemareel_Poster, link:"https://cinema-reel.netlify.app/", text:"CinemaReel is a user-friendly, responsive platform for discovering trending films. Users can explore detailed information about each movie and seamlessly navigate to its IMDb page via provided links. The platform boasts a clean, intuitive interface complemented by a fully functional search bar for easy navigation."}
+  const ProjectsList = [Project1, Project2, Project3, Project4]
 
-  const handleCardClick = (Project) =>{
+  const handleClickFocus = (Project) =>{
     selectedProject?.id === Project.id ? setselectedProject(null) : setselectedProject(Project)
   }
 
@@ -29,7 +33,9 @@ const Projects = () => {
               <ProjectCard
                 key={Project.id} 
                 props={Project}
-                onClick={()=>handleCardClick(Project)}
+                onClick={()=> isTouchable && handleClickFocus(Project)}
+                onMouseEnter={()=> {!isTouchable && setselectedProject(Project)}}
+                onMouseLeave={()=> {!isTouchable && setselectedProject(null)}}
                 isFocused={selectedProject?.id === Project.id}
               />
           ))}
--- src/utilities/ProjectCard.jsx
+++ src/utilities/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import IsTouchable from "./TouchContext";
 
-const ProjectCard = ({ props, onClick, isFocused }) => {
+const ProjectCard = ({ props, onClick, onMouseEnter, onMouseLeave, isFocused }) => {
   const [maxPermissibleWidth, setmaxPermissibleWidth] = useState(0)
   const isTouchable = IsTouchable();
 
@@ -24,19 +24,26 @@ const ProjectCard = ({ props, onClick, isFocused }) => {
   return (
     <div 
       id="PROJECT CARD" 
-      onClick={onClick} 
-      className={`flex flex-col justify-center items-center p-3 cursor-pointer rounded-2xl ${isTouchable ? 'bg-opacity-20 bg-cyan-900 shadow-2xl min-w-fit' : 'bg-opacity-20 bg-cyan-900 shadow-xl hover:bg-opacity-40 hover:shadow-2xl transition-all md:flex-row'} ${isFocused && 'bg-opacity-40 shadow-2xl md:px-4 min-w-80'}`}
+      onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      className={`flex flex-col justify-center items-center p-3 rounded-2xl 
+      ${isTouchable ? 'bg-opacity-20 bg-cyan-900 shadow-2xl min-w-fit' : 'bg-opacity-20 bg-cyan-900 shadow-xl hover:bg-opacity-30 transition-all duration-300 ease-in-out md:flex-row'} 
+      ${isFocused && 'bg-opacity-40 shadow-2xl md:px-4 min-w-80 transition-all duration-300'}`}
     >
-      <div className={`relative overflow-hidden transition-all duration-300 md:duration-200 ease-in-out md:ease-in text-cyan-600 ${!isTouchable && (isFocused ? 'max-w-[36vw] h-40 delay-100' : 'max-w-0 h-40')} ${isTouchable && (isFocused ? 'max-h-[500px] w-72 delay-100' : 'max-h-0 w-72') }`}>
+      <div className={`relative overflow-hidden transition-all duration-500 ease-in-out text-cyan-600 
+        ${!isTouchable && (isFocused ? 'max-w-[36vw] h-40 opacity-100' : 'max-w-0 h-40 opacity-0')} 
+        ${isTouchable && (isFocused ? 'max-h-[500px] w-72 delay-100' : 'max-h-0 w-72')}`}>
         <h1 className="text-cyan-400 font-semibold">{props.title}</h1>
         <hr className="p-1 border-cyan-200"/>
         <p className="pb-2 md:pr-2 mb-8 tracking-tight leading-[1.3rem]">{props.text}</p>
         <div className={`absolute bottom-1 w-full ${isTouchable && 'mt-2'}`}>
-          <a href={props.link} className="rounded-xl px-3 py-1 font-semibold text-sm text-[#0a192f] bg-cyan-600 hover:bg-[#0a192f] hover:text-cyan-600 transition-all duration-100 ease-in-out">VISIT SITE!</a>
+          <a href={props.link} className="rounded-xl px-3 py-1 font-semibold text-sm text-[#0a192f] bg-cyan-600 hover:bg-[#0a192f] hover:text-cyan-600 transition-all duration-300 ease-in-out">VISIT SITE!</a>
         </div>
       </div>
-      <div className={`flex flex-col gap-2 items-center justify-center transition-all duration-300 ease-in-out overflow-hidden ${isTouchable? (isFocused && 'mt-4') : (isFocused ? 'w-0 opacity-0' : 'w-72')}`}>
-        <img src={props.poster} alt={props.title} className={`rounded-xl`}/>
+      <div className={`flex flex-col gap-2 items-center justify-center transition-all duration-500 ease-in-out overflow-hidden 
+        ${isTouchable ? (isFocused && 'mt-4') : (isFocused ? 'w-0 opacity-0 scale-95' : 'w-72 opacity-100 scale-100')}`}>
+        <img src={props.poster} alt={props.title} className={`rounded-xl transition-all duration-300 ${!isTouchable && 'hover:scale-105'}`}/>
         <h1 className={`text-cyan-600 font-semibold tracking-wide transition-all duration-300 ease-in-out ${isTouchable && isFocused ? 'h-0' : 'h-5'}`}>{props.title}</h1>
       </div>
     </div>
